af12d2cceafa5f5087e84219436a957fd0aaa561
unknown <talukdargour.bd@gmail.com>
2026-06-26 23:56:29 +0600

    final commit

--- _read_pdf.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfReader
-reader = PdfReader(r'c:\Users\Rosen\Downloads\SUST_Hackathon_Preli_Problem_Statement.pdf')
-with open(r'd:\SUST CSE CARNIVAL\Preli\CSECar-SWE_TARGARYENS\_pdf_out.txt', 'w', encoding='utf-8') as f:
-    for i, page in enumerate(reader.pages):
-        f.write(f'--- PAGE {i+1} ---\n')
-        f.write(page.extract_text())
-        f.write('\n')
-print('OK')
